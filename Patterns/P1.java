eb7dc14788a2a66b23d6d16045d6a3902dbd1bab
Madihaj14 <madihajk14@gmail.com>
2024-08-24 09:12:42 +0530

    Patterns problem 1

---
+++ Patterns/P1.java
@@ -0,0 +1,27 @@
+package JavaPreparation.Patterns;
+
+public class P1 {
+    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       // for (int counter = 0; counter<11; counter++){
+         //   System.out.println(counter);
+        //}
+        int n = 4;
+        int m = 5;
+        //outer loop
+        for (int i=1; i<=n; i++){
+            //inner loop
+            for (int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+}
+   
