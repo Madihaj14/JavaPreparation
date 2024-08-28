0acd81efa7aaf1833abcd4834d6ef6b9f2172934
Madihaj14 <madihajk14@gmail.com>
2024-08-28 11:39:59 +0530

    inverted hald pyramid rotated by 180 deg

--- Patterns/P4.java
+++ Patterns/P4.java
@@ -6,8 +6,13 @@ public class P4 {
         int n = 4;
 
         //outer loop
-        for (int i=n; i>=1; i--){
-            //inner loop
+        for (int i=1; i<=n; i++) {
+            //inner loop --> Space print
+            for (int j=1; j<=n-1; j++){
+                System.out.print(" ");
+            }
+
+            //inner loop --> Star print
             for (int j=1; j<=i; j++) {
                 System.out.print("*");
             }
