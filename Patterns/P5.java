9d0c2eb90286be8e3c7ee086c5d31927fb55375e
Madihaj14 <madihajk14@gmail.com>
2024-08-29 23:29:12 +0530

    update

--- Patterns/P5.java
+++ Patterns/P5.java
@@ -10,8 +10,6 @@ public class P5 {
         //inner loop
             for (int j=1; j<=i; j++) {
                 System.out.print(j);        
-            }
-            System.out.println();
-        }
+          
     }
 }
