63e879c836f42e835e7b65c0fbda3ca6f54d6648
Madihaj14 <madihajk14@gmail.com>
2024-08-30 23:07:47 +0530

    new test

--- Patterns/P5.java
+++ Patterns/P5.java
@@ -3,7 +3,7 @@ package JavaPreparation.Patterns;
 public class P5 {
     public static void main(String args []) {
 
-        int n = 4;
+        int n = 5;
         //half pyramid with numbers
         //outer loop
         for (int i=1; i<=n; i++) {
