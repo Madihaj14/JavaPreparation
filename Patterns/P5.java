da1616b0414a4075abe64d31109bc6ac552dc285
Madihaj14 <madihajk14@gmail.com>
2024-08-31 08:24:11 +0530

    clean code

--- Patterns/P5.java
+++ Patterns/P5.java
@@ -9,7 +9,7 @@ public class P5 {
         for (int i=1; i<=n; i++) {
         //inner loop
             for (int j=1; j<=i; j++) {
-                System.out.print(j);        
+                System.out.print(j+" ");        
             }
             System.out.println();
         }
