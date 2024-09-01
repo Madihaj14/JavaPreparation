6f67cb5ff5686236f554619adeedbb3f4670ed71
Madihaj14 <madihajk14@gmail.com>
2024-09-01 10:01:35 +0530

    solid rhombus pattern

---
+++ AdvPatterns/AP2.java
@@ -0,0 +1,21 @@
+package JavaPreparation.AdvPatterns;
+
+public class AP2 {
+    public static void main(String[] args) {
+        //SOLID RHOMBUS
+        int n = 5;
+        
+        for (int i=1; i<=n; i++) {
+            //spaces
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j=1; j<=5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
