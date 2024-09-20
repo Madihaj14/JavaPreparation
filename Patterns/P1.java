bf2f0d5e9b0e67a23ddc8c97f20823e3d63c01f5
Madihaj14 <madihajk14@gmail.com>
2024-09-20 23:21:51 +0530

    new update

--- Patterns/P1.java
+++ Patterns/P1.java
@@ -11,13 +11,7 @@ public class Main {
         int n = 4;
         int m = 5;
         //outer loop
-        for (int i=1; i<=n; i++){
-            //inner loop
-            for (int j=1; j<=m; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+       
 
     }
 }
