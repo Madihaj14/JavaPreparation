2e1c52c22bc495c61f4a419d0c3771f1ee0f8d2b
Madihaj14 <madihajk14@gmail.com>
2024-09-24 05:08:40 +0530

    learning fuctions

---
+++ Functions/Func1.java
@@ -0,0 +1,14 @@
+package JavaPreparation.Functions;
+
+public class Func1 {
+
+    public static void printMyName (String name ) {
+        System.out.println(name);
+        return;
+        
+    }
+    public static void main (String args[]) {
+
+    }
+    
+}
