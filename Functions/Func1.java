860081c323efd6ae42fbfcf96ddcaaf0c0df491b
Madihaj14 <madihajk14@gmail.com>
2024-09-24 05:12:18 +0530

    final function code

--- Functions/Func1.java
+++ Functions/Func1.java
@@ -1,13 +1,18 @@
 package JavaPreparation.Functions;
+import java.util.*;
 
 public class Func1 {
 
     public static void printMyName (String name ) {
         System.out.println(name);
         return;
-        
+
     }
     public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name);//call kiya function ko
 
     }
     
