85dc4e471fb606382e8813cf55a9a08f6bce84fb
Madihaj14 <madihajk14@gmail.com>
2024-09-23 23:04:59 +0530

    update code

--- Patterns/P7.java
+++ Patterns/P7.java
@@ -7,13 +7,6 @@ public class P7 {
         int number = 1;
         //Floyd's triangle
         //outer loop
-        for (int i=1; i<=n; i++) {
-        //inner loop
-            for (int j=1; j<=i; j++) {
-                System.out.print(number + " ");
-                number++; //number = number + 1
-            }
-            System.out.println();
-        }
+        
     }
 }
