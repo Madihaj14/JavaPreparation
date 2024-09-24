588acca45f565c119811ccac344c048aadaa8009
Madihaj14 <madihajk14@gmail.com>
2024-09-24 05:38:17 +0530

    new question

---
+++ Functions/Func3.java
@@ -0,0 +1,21 @@
+package JavaPreparation.Functions;
+
+import java.util.Scanner;
+
+//Que: Make a fuction to multiply two numbers and return the product
+public class Func3  {
+    public static int calculateProduct (int a, int b) {
+        return a* b;
+    } 
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Product of two nos. is: " + calculateProduct(a, b));
+        sc.close ();
+
+
+    }
+    
+}
