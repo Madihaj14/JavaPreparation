9718cd81e1e0b510df1301e5802de6a448e9b8e9
Madihaj14 <madihajk14@gmail.com>
2024-10-24 04:56:16 +0530

    arrays practice code

--- Arrays/Arr1.java
+++ Arrays/Arr1.java
@@ -12,17 +12,11 @@ public class Arr1 {
         for (int i=0; i<size; i++) {
             numbers[i] = sc.nextInt();
         }
-        // int[] marks = new int[3];
-        // marks[0] = 97; //phy
-        // marks[1] = 98; //chem
-        // marks[2] = 95; //eng
-        // System.out.println(marks[0]);
-        // System.out.println(marks[1]);
-        // System.out.println(marks[2]);
+    
 
         //array can be printed in loops(output)
         for (int i=0; i<3; i++) {
-            System.out.println(marks[i]);
+            System.out.println(numbers[i]);
         }
         sc.close();
     }
