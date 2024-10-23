66ba0e7ed0b0fb11464e2c2a735075f9aa90295a
Madihaj14 <madihajk14@gmail.com>
2024-10-24 04:51:04 +0530

    new file

--- Arrays/Arr1.java
+++ Arrays/Arr1.java
@@ -4,18 +4,27 @@ import java.util.*;
 
 public class Arr1 {
     public static void main(String args[]) {
-        int[] marks = new int[3];
-        marks[0] = 97; //phy
-        marks[1] = 98; //chem
-        marks[2] = 95; //eng
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        //input
+        for (int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        // int[] marks = new int[3];
+        // marks[0] = 97; //phy
+        // marks[1] = 98; //chem
+        // marks[2] = 95; //eng
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
         // System.out.println(marks[2]);
 
-        //array can be printed in loops
+        //array can be printed in loops(output)
         for (int i=0; i<3; i++) {
             System.out.println(marks[i]);
         }
+        sc.close();
     }
     
 }
