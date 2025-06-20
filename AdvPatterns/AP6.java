d0e2b71f645e2a7c328612028448ffc7d09d283c
Madihaj14 <madihajk14@gmail.com>
2025-06-20 23:36:09 +0530

    new pattern problem

---
+++ AdvPatterns/AP6.java
@@ -0,0 +1,43 @@
+package JavaPreparation.AdvPatterns;
+
+public class AP6 {
+    public static void main(String[] args) {
+        
+        int n = 4;
+        //outer loop, upper half
+        for (int i=1; i<=n; i++) {
+
+            //innerloop
+            //spaces
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+        //lower half, outer loop
+        for (int i=n; i>=1; i--) {
+
+            //innerloop
+            //spaces
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            //stars
+            for (int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+    }
+    
+}
