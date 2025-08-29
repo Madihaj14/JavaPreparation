3bde14e9ce064eb5ba4af6d2ad490d763f811f4c
Madihaj14 <madihajk14@gmail.com>
2025-08-29 23:51:27 +0530

    que 1 solved

--- Exercise1/Que1.java
+++ Exercise1/Que1.java
@@ -2,5 +2,17 @@ package JavaPreparation.Exercise1;
 
 public class Que1 {
     //Enter 3 numbers from the user & make a function to print their average
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        System.out.println("The average of three numbers is: " + average(a, b, c));
+    }
+
+    public static double average(int x, int y, int z) {
+        return (x + y + z) / 3.0;
+    }
+
     
+
 }
