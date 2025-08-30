28e2018ee2fa3dd0a94a35d96ab7d793591064a7
Madihaj14 <madihajk14@gmail.com>
2025-08-30 23:58:26 +0530

    que 1 notes updated

--- Exercise1/Que1.java
+++ Exercise1/Que1.java
@@ -8,7 +8,8 @@ public class Que1 {
         int c = 30;
         System.out.println("The average of three numbers is: " + average(a, b, c));
     }
-
+    
+    // Function to calculate average of three numbers
     public static double average(int x, int y, int z) {
         return (x + y + z) / 3.0;
     }
