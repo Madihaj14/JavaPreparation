10ef042e437c9236da758e5ad6c3d1440269dc5e
Madihaj14 <madihajk14@gmail.com>
2025-08-31 23:10:21 +0530

    notes updated with practice question

--- Prac2/Code.java
+++ Prac2/Code.java
@@ -2,8 +2,10 @@ package JavaPreparation.Prac2;
 
 import java.util.*;
 
+
+// WAP to take two numbers as input and print whether they are equal or not. If not, print which one is greater.
 public class Code {
-    
+
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
