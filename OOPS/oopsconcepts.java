24b8e23881da7c2c1d86ecf842665dcbc8fed6b1
Madihaj14 <madihajk14@gmail.com>
2025-09-07 22:57:36 +0530

    Updated OOPs notes

--- OOPS/oopsconcepts.java
+++ OOPS/oopsconcepts.java
@@ -550,22 +550,6 @@ public class OOPS {
 
 /*
  Object class equals to string hashcode() method
-    public class OOPS {
-        public static void main(String args[]) {
-            String s1 = new String("Hello");
-            String s2 = new String("Hello");
-    
-            System.out.println(s1.equals(s2)); // true
-            System.out.println(s1 == s2); // false
-    
-            System.out.println(s1.hashCode()); // same hashcode
-            System.out.println(s2.hashCode()); // same hashcode
-    
-            System.out.println(System.identityHashCode(s1)); // different hashcode
-            System.out.println(System.identityHashCode(s2)); // different hashcode
-        }
-
-        
  */
 
 
