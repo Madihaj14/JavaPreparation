dd5b1afd4bbf300f21b5cb8b464b5c68c960e9bf
Madihaj14 <madihajk14@gmail.com>
2025-09-09 23:21:45 +0530

    updated notes

--- Patterns/P1.java
+++ Patterns/P1.java
@@ -1,5 +1,3 @@
-package JavaPreparation.Patterns;
-
 public class P1 {
     //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
