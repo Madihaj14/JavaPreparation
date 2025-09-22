f8769dd85ca37a33766123799a4f4f18a720fae3
Madihaj14 <madihajk14@gmail.com>
2025-09-22 09:46:57 +0530

    collections notes updated

--- Collection/Notes.java
+++ Collection/Notes.java
@@ -6,10 +6,24 @@ public class Notes {
      * Collection is a root interface in the Collection API that represents a group of objects.
      * Collections is a utility class that provides static methods for operating on collections.
      * Uses of Collections class include sorting, searching, and shuffling elements in a collection.
+     * Why Collection is an interface and Collections is a class?
+     * Collection is an interface because it defines a contract for a group of objects without providing implementation.
+     * Collections is a class because it provides concrete implementations of utility methods for collections.
+     * Collections class is final, meaning it cannot be subclassed.
+     * Collections class has a private constructor to prevent instantiation, as it is meant to be used in a static context.
+     * Collections class provides static methods that operate on or return collections, such as sorting and searching.
+     * Collections class is part of the java.util package, which contains the Collection API.
+     * Collections class is designed to work with the Collection interface and its subinterfaces, such as List, Set, and Queue.
+     * Collections class provides methods that can be used with any implementation of the Collection interface, such
+     * as ArrayList, HashSet, and LinkedList.
+     * Collections class provides methods that can be used with any type of object, as it uses
+     * 
+     * 
+     * 
      * 
      */
 
 
 
-     
+
 }
