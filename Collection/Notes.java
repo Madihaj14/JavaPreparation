6efd4b7b8031f788038a8643c478e15f7273fe69
Madihaj14 <madihajk14@gmail.com>
2025-09-22 09:30:33 +0530

    collections framework added

---
+++ Collection/Notes.java
@@ -0,0 +1,15 @@
+public class Notes {
+    
+    /*
+     * Difference between Collection API, Collection and Collections
+     * Collection API is a framework that provides architecture to store and manipulate a group of objects.
+     * Collection is a root interface in the Collection API that represents a group of objects.
+     * Collections is a utility class that provides static methods for operating on collections.
+     * Uses of Collections class include sorting, searching, and shuffling elements in a collection.
+     * 
+     */
+
+
+
+     
+}
