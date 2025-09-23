f2112ec8c9616152ec356c4ea91122f1c99fd115
Madihaj14 <madihajk14@gmail.com>
2025-09-23 23:27:39 +0530

    arraylist notes updated

---
+++ Arrays/ArrayList.java
@@ -0,0 +1,16 @@
+public class ArrayList {
+    
+    // Main method
+    public static void main(String[] args) {    
+        // Create an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the elements of the array
+        System.out.println("Array elements:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    
+}
