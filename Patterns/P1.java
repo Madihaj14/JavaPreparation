25f61679306e44835b92591ae3e75f17fef8b6a2
Madihaj14 <madihajk14@gmail.com>
2025-10-06 23:08:10 +0530

    notes updated

--- Patterns/P1.java
+++ Patterns/P1.java
@@ -1,6 +1,7 @@
 public class P1 {
-    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    
+
+    // Pattern 1: Rectangle of Stars
 public class Main {
     public static void main(String[] args) {
        // for (int counter = 0; counter<11; counter++){
