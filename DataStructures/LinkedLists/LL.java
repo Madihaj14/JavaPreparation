dc2c52e9a354d64143da9b2eb85b85e9a0a58db8
Madihaj14 <madihajk14@gmail.com>
2025-10-25 23:55:35 +0530

    notes updated

--- DataStructures/LinkedLists/LL.java
+++ DataStructures/LinkedLists/LL.java
@@ -2,14 +2,14 @@ import java.util.*;
 
         //Linked Lists in Java Collections Framework (JCF)
         /*
-            * A linked list is a linear data structure where each element is a separate object.
-            * Each element (we will call it a node) of a list is comprising of two items - the data and a reference to the next node.
-            * The last node has a reference to null. The entry point into a linked list is called the head of the list.
-            * If the list is empty then the head is a null reference.
-            * A linked list is a dynamic data structure. It can grow and shrink at runtime by allocating and deallocating memory.
-            * In Java, LinkedList class is a part of java.util package.
-            * It implements the List and Deque interfaces. It can be used as a list, stack or queue.
-            * It maintains the insertion order and is not synchronized.
+        A linked list is a linear data structure where each element is a separate object.
+        Each element (we will call it a node) of a list is comprising of two items - the data and a reference to the next node.
+        The last node has a reference to null. The entry point into a linked list is called the head of the list.
+        If the list is empty then the head is a null reference.
+        A linked list is a dynamic data structure. It can grow and shrink at runtime by allocating and deallocating memory.
+        In Java, LinkedList class is a part of java.util package.
+        It implements the List and Deque interfaces. It can be used as a list, stack or queue.
+        It maintains the insertion order and is not synchronized.
             
          */
 
