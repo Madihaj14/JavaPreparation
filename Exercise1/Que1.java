c9a6319028be1575fbf0b34aae80009dbe7b9f9e
Madihaj14 <madihajk14@gmail.com>
2025-10-26 23:52:30 +0530

    notes updated

--- Exercise1/Que1.java
+++ Exercise1/Que1.java
@@ -1,14 +1,18 @@
 package JavaPreparation.Exercise1;
 
 public class Que1 {
-    //Enter 3 numbers from the user & make a function to print their average
+    //Take 3 numbers from the user as input & make a function to print their average
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 30;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
         System.out.println("The average of three numbers is: " + average(a, b, c));
     }
-    
+
     // Function to calculate average of three numbers
     public static double average(int x, int y, int z) {
         return (x + y + z) / 3.0;
