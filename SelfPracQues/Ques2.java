ee3541e3798b734705940302eae8a6752819ba7d
Madihaj14 <madihajk14@gmail.com>
2025-11-08 02:46:18 +0530

    started working on the second question

---
+++ SelfPracQues/Ques2.java
@@ -0,0 +1,11 @@
+public class Ques2 {
+    
+    //Write a java function to convert any integer to roman letters. input: 23 , output: XXII
+
+    /*
+    A rough approach can be that we first define the symbols for each roman number 
+    and then establsh a relationship between the decimal numbers and roman numbers.
+    
+     */
+
+}
