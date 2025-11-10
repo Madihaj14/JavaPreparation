2089bbd24eafbfe067f6006fcddda1d940621df9
Madihaj14 <madihajk14@gmail.com>
2025-11-10 23:14:08 +0530

    stack implementaion notes

--- DataStructures/Stacks/Notes.java
+++ DataStructures/Stacks/Notes.java
@@ -15,6 +15,66 @@ public class Notes {
         e. search(): to search for an element in the stack and return its position from the top
      */
 
+
+    //Stack implementation in Java from scratch
+    static class Stack<T> {
+        private Node<T> top;
+
+        private static class Node<T> {
+            T data;
+            Node<T> next;
+
+            Node(T data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        // Push method to add an element to the top of the stack
+        public void push(T data) {
+            Node<T> newNode = new Node<>(data);
+            newNode.next = top;
+            top = newNode;
+        }
+
+        // Pop method to remove and return the top element of the stack
+        public T pop() {
+            if (isEmpty()) {
+                throw new IllegalStateException("Stack is empty");
+            }
+            T data = top.data;
+            top = top.next;
+            return data;
+        }
+
+        // Peek method to view the top element without removing it
+        public T peek() {
+            if (isEmpty()) {
+                throw new IllegalStateException("Stack is empty");
+            }
+            return top.data;
+        }
+
+        // Method to check if the stack is empty
+        public boolean isEmpty() {
+            return top == null;
+        }
+
+        // Search method to find the position of an element from the top of the stack
+        public int search(T data) {
+            Node<T> current = top;
+            int position = 1;
+            while (current != null) {
+                if (current.data.equals(data)) {
+                    return position;
+                }
+                current = current.next;
+                position++;
+            }
+            return -1; // Element not found
+        }
+    }
+
     /*
     Example usage of Stack class is shown below.
      */
