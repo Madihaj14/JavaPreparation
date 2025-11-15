28fff843675c69ae3158700a99e08d054d1c0384
Madihaj14 <madihajk14@gmail.com>
2025-11-15 23:46:05 +0530

    started solving by defining the intution

--- SelfPracQues/Ques2.java
+++ SelfPracQues/Ques2.java
@@ -1,7 +1,12 @@
 public class Ques2 {
     
     //Write a java function to convert any integer to roman letters. input: 23 , output: XXII
-
+    /*
+    Intution: I am first gonna define each symbol for the integer values in two arrays. One for the integer values and one for the corresponding roman letters.
+    Then I will iterate through the integer values array from the largest to the smallest value. For each value, I will determine how many times it fits into the input integer.
+    I will append the corresponding roman letters to the result string that many times and subtract the total value from the input integer.
+    I will continue this process until the input integer is reduced to zero.
+     */
 
   
 
