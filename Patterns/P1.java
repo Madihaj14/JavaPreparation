d869ddb86842c70b3b28399732cca00e2d437f02
Madihaj14 <madihajk14@gmail.com>
2025-12-05 09:27:26 +0530

    practicing pattern problems

--- Patterns/P1.java
+++ Patterns/P1.java
@@ -1,26 +1,36 @@
 public class P1 {
     
 
-    // Pattern 1: Rectangle of Stars
-public class Main {
-    public static void main(String[] args) {
-       // for (int counter = 0; counter<11; counter++){
-      //   System.out.println(counter);
-        //}
-        int n = 4;
-        int m = 5;
-        //outer loop
-        for (int i=1; i<=n; i++){
-            //inner loop
-            for (int j=1; j<=m; j++){
-                System.out.print("*");
+    /* Pattern 1: Rectangle of Stars
+            *****
+            *****
+            *****
+            *****
+    */
+    public class Main {
+        public static void main(String[] args) {
+            
+        /* for (int counter = 0; counter<11; counter++){
+                        System.out.println(counter); 
+        } */
+
+            int n = 4;
+            int m = 5;
+
+            //outer loop for rows
+            for (int i=1; i<=n; i++){
+
+                //inner loop for columns and print stars
+
+                for (int j=1; j<=m; j++){
+                    System.out.print("*");
+                }
+
+                System.out.println(); //new line after each row
             }
-            System.out.println();
+
         }
-
     }
-}
+}    
 
-
-}
    
