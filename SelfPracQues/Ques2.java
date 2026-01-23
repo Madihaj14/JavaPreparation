e046ef6291afd239f93421aed70077909371cf7c
Madihaj14 <madihajk14@gmail.com>
2026-01-23 20:14:23 +0530

    notes updated

--- SelfPracQues/Ques2.java
+++ SelfPracQues/Ques2.java
@@ -10,8 +10,19 @@ public class Ques2 {
 
     public static String convertToRoman(int num ) {
 
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] intValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-        
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < intValues.length; i++) {
+            while (num >= intValues[i]) {
+                result.append(romanNumerals[i]);
+                num -= intValues[i];
+            }
+        }
+
+        return result.toString();
     }
   
 
